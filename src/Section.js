1e491d418198577dd5be6dad0478db26d0779035
Hugo Heneman <hugo.heneman@yahoo.fr>
2019-06-19 16:50:37 +0200

    2eme Jour Animation/Transition W/D

--- src/Section.js
+++ src/Section.js
@@ -1,77 +1,115 @@
-import React, { Component,Fragment } from 'react'
-import Background from './assets/img/431.jpg';
-import Quote from './assets/img/quote.png';
-import Tache from './assets/img/ink_corner.svg'
-import './Section.css'
+import React, { Component, Fragment } from "react";
+import Background from "./assets/img/431.jpg";
+import Quote from "./assets/img/quote.png";
+import Tache from "./assets/img/ink_corner.png";
+import TacheWhite from "./assets/img/ink_corner_white.png"
+import "./Section.css";
+import ReactResizeDetector from 'react-resize-detector';
 
 const sectionStyle = {
   width: "100%",
   height: "auto",
-  minHeight:"1077px",
-  backgroundImage: 'url('+Background+')',
+  minHeight: "1077px",
+  backgroundImage: "url(" + Background + ")",
   padding: "0"
 };
 const quoteStyle = {
-  marginTop : "15%",
-  transform: "scale(1.3)"
+  margin : "0 auto",
+  marginTop: "15%",
+  transform: "scale(1.3)",
+  position: "absolute",
+  marginLeft : "-12%",
 };
 const tacheStyle = {
-  position : "absolute",
+  position: "absolute",
   right: "-600px",
   top: "-500px",
   transform: "scale(0.5)",
-  position : "fixed"
-}
-const opening = {
-/*   animation: "cd-sequence 0.8s steps(24)",
-  animation-fill-mode: forwards, */
+  position: "fixed",
+  zIndex : 4,
+};
 
-}
-const closing = {
- /* animation: "cd-sequence-reverse 0.8s steps(24)",*/
-  
-}
 
 class Section extends React.Component {
-    state = {
-      isVisible : 2,
-      classDiv : "cd-transition-layer visible",
-    }
+  state = {
+    isVisible: 2,
+    animationisplaying: 0,
+    classDiv: "cd-transition-layer visible"
+  };
 
-    Clickopen(e) {
-      e.preventDefault()
-      if(this.state.isVisible=== 2){
-      this.setState({isVisible : 1})}
-      if(this.state.isVisible=== 0){
-        this.setState({isVisible : 2})}
-      if(this.state.isVisible=== 1){
-        this.setState({isVisible : 0})
-        this.TransitionEvent()
-        setInterval(this.setState({isVisible : 2}), 2000)
-      }
-      this.TransitionEvent()
-    }
-
-    TransitionEvent() {
-      if(this.state.isVisible==2) this.state.classDiv = "cd-transition-layer visible"
-      if(this.state.isVisible==1) this.state.classDiv = "cd-transition-layer visible opening"
-      if(this.state.isVisible==0) this.state.classDiv = "cd-transition-layer visible closing"
-    }
-
-    render() {
-      console.log(this.state.isVisible)
-      return (
-        <div className={this.state.classDiv}>
-          <div className="section1" style={ sectionStyle }>
-            <img src={Background} /> 
-            {/* <img src={Quote} style={ quoteStyle }></img> */}
-            <img src={Tache} style={tacheStyle}></img>
-            <div onClick={(e)=>this.Clickopen(e)} className="cd-btn cd-modal-trigger">APPUIE</div>
-          </div>
-        <div className="bg-layer"></div>
-         </div>
-      );
-    }
+  Clickopen(e) {
+    e.preventDefault();
+    this.setState({ animationisplaying: 1 });
   }
 
-  export default Section
+  
+  Clickclose(e) {
+    e.preventDefault();
+    this.setState({ animationisplaying: 2 });
+  }
+
+  EventDelay() {
+    this.setState({ isVisible: 2 });
+  }
+
+  TransitionEvent() {
+    console.log(this.state.isVisible);
+    if (this.state.isVisible == 2)
+      this.state.classDiv = "cd-transition-layer visible";
+    if (this.state.isVisible == 1)
+      this.state.classDiv = "cd-transition-layer visible opening";
+    if (this.state.isVisible == 0)
+      this.state.classDiv = "cd-transition-layer visible closing";
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <div className={this.state.animationisplaying===0? "cd-modal" : this.state.animationisplaying===1? "cd-modal visible" : this.state.animationisplaying===2? "cd-modal" : ""}>
+	<div className="modal-content">
+		<h1>My Modal Content</h1>
+		
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad modi repellendus, optio eveniet eligendi molestiae? Fugiat, temporibus! A rerum pariatur neque laborum earum, illum voluptatibus eum voluptatem fugiat, porro animi tempora? Sit harum nulla, nesciunt molestias, iusto aliquam aperiam est qui possimus reprehenderit ipsam ea aut assumenda inventore iste! Animi quaerat facere repudiandae earum quisquam accusamus tempora, delectus nesciunt, provident quae aliquam, voluptatum beatae quis similique in maiores repellat eligendi voluptas veniam optio illum vero! Eius, dignissimos esse eligendi veniam.
+		</p>
+
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis saepe amet sit fugit rerum, corporis minus vitae officia quaerat incidunt voluptate, blanditiis ea est quibusdam voluptas animi quasi totam magni, commodi praesentium. Possimus quam illo ipsam iste unde totam cupiditate deleniti, impedit assumenda hic eligendi natus tempora dolores quod mollitia ab non sunt eaque adipisci, suscipit quas aliquid officiis beatae. Necessitatibus voluptatibus, perferendis tenetur perspiciatis adipisci nesciunt eum ex fuga commodi iure numquam enim rem ullam labore nisi magni sint voluptatem quos! Eum iure exercitationem voluptates repellendus culpa doloremque laborum animi illum, sint fugit soluta possimus a fuga veritatis molestias corporis placeat illo pariatur dolor reiciendis earum, sapiente omnis. Placeat maiores omnis, porro officia, laborum eos. Fugiat mollitia inventore consequuntur odit eaque, rerum recusandae, eum sint molestiae consequatur culpa deserunt quae aliquid dolor tempora tenetur architecto repellendus enim quasi atque, odio voluptas. Tenetur repellendus explicabo ipsum inventore quia aut eos expedita necessitatibus asperiores blanditiis! Delectus nisi laudantium ipsum! Quasi blanditiis corrupti dicta maiores placeat laboriosam delectus ipsum facere voluptas, magnam voluptatibus, perferendis alias ullam saepe, perspiciatis recusandae voluptates, dolores praesentium?
+		</p>
+	</div>
+  </div>
+      <div
+        className={
+          this.state.animationisplaying===0
+            ? "cd-transition-layer visible" :  this.state.animationisplaying===1
+            ? "cd-transition-layer visible opening" :   this.state.animationisplaying===2
+            ? "cd-transition-layer visible opening closing" : ""
+        }
+      >
+      <img src={Quote} style={ quoteStyle }></img>
+        <div className="section1light" style={sectionStyle}>
+          <img src={Background} />
+          <img className={this.state.animationisplaying===1? "invisible" : "visible"} src={Tache} style={tacheStyle} />
+          <img className={this.state.animationisplaying===1? "visible" : "invisible"} src={TacheWhite} style={tacheStyle} />
+          <div style={{position:"fixed"}}
+            onClick={e => this.Clickopen(e)}
+            className={this.state.animationisplaying===1? "cd-btn cd-modal-trigger" : "cd-btn cd-modal-trigger visible"}
+          >
+            
+          </div>
+          <div
+          style={{position:"fixed"}}
+            onClick={e => this.Clickclose(e)}
+            className={this.state.animationisplaying===1? "cd-btn cd-modal-trigger visible" : "cd-btn cd-modal-trigger"}
+          >
+            
+          </div>
+        </div>
+        <div className="bg-layer" />
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Section;
