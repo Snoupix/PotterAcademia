10122ed50fe6214b35f693de161a4c95c06524c4
Corentin <corentin-agneray@orange.fr>
2019-06-24 14:30:53 +0200

    pareil

--- src/Bouton_fleche.js
+++ src/Bouton_fleche.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
-import scrollTop from 'react-scroll'
 
 
 const flechestyle ={
