a0eb7d247ff20a5d88854824b892f22c8751f2c0
Snoupix <samufpsdu59@hotmail.fr>
2019-06-29 22:44:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Section from './Section'
 import Bouton_fleche from './Bouton_fleche'
-import Section from './Section';
 
 import NavbarLight from './navbarLight';
 import NavbarDark from './navbarDark';
